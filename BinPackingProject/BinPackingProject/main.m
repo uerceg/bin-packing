d7bde10337be2446bbfb4dc50b6320fabaf8c63c
Ugljesa Erceg <UERCEG@UERCEG-MACBOOK.local>
2012-05-15 14:46:01 +0200

    Initial GitHub project commit.

---
+++ BinPackingProject/BinPackingProject/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  BinPackingProject
+//
+//  Created by Ugljesa Erceg on 5/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
